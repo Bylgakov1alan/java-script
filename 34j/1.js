111c5da157825412cb78ef382a2088bafdeb1a81
Alan <bulgakov.alan@list.ru>
2023-03-08 11:02:40 +0300

    «j34»

---
+++ 34j/1.js
@@ -0,0 +1,75 @@
+//z1
+let z11 = 1;
+let z12 = 2;
+console.log('сумма');
+console.log(z11 + z12); 
+
+//z2
+ let a = 9;
+let b = 6;
+console.log(a + b);
+
+//z3
+let n1 = '123';
+let s1 = Number(n1[0]) + Number(n1[1]) + Number(n1[2]);
+console.log(s1);
+
+//z4
+let n2 = '123';
+console.log(n2[0]);
+
+
+//z5
+let a1 = 0;
+console.log(++a1);
+
+
+
+//z6
+let n3 = '123';
+console.log(n3.length);
+
+
+//z7
+let a2 = 24 / 60 / 60;
+console.log(a2);
+
+
+//z8
+let numr = 123;
+let str = String(numr);
+console.log(str.length);
+
+
+//z9
+let n7 = 123;
+let str7 = String(n7);
+console.log(str7[str7.length-1]);
+
+
+//z10
+let num8 = 123;
+let str8 = String(num8);
+console.log(str8.length);
+
+
+//z11
+let num9 = 123;
+let str9 = String(num9);
+console.log(str9[str9.length-2]);
+
+
+
+//z12
+let a6 = '123';
+let b6 = '456';
+let s = Number(a6) + String(b6);
+console.log(s);
+
+
+//z13
+let aaa = 1;
+let bbb = 2;
+let ccc = 3;
+let ssss=aaa+bbb+ccc;
+console.log(ssss);
