77fbba8481b4c1f33c8a24ee0c79b37d736127a3
Alan <bulgakov.alan@list.ru>
2023-03-08 10:36:40 +0300

    «j32»

---
+++ 32j/1.js
@@ -0,0 +1,19 @@
+alert(0.1 * 0.2); //0.020000000000000004
+alert(0.3 - 0.1); //0.19999999999999998
+
+let imya=prompt('введите имя')
+alert('введите имя: '+imya);
+
+сумма
+let num1=prompt('введите 1 число');
+let num2=prompt('введите 2 число');
+alert(Number(num1)+Number(num2));
+//площадь квадрата
+let kv=prompt('введите сторону квадрата');
+alert('площадь квадрата= '+Number(kv)*Number(kv));
+//перимтр треугольника
+let tr=prompt('введите 1 сторону треугольника');
+let tr1=prompt('введите 2 сторону треугольника');
+let tr2=prompt('введите 3 сторону треугольника');
+let P=Number(tr)+Number(tr1)+Number(tr2);
+alert('периметр треугольника= '+P)
