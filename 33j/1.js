a891fbaf88216b64ea95dbc5dc583af8244e2ce5
Alan <bulgakov.alan@list.ru>
2023-03-08 10:45:26 +0300

    «j33»

---
+++ 33j/1.js
@@ -0,0 +1,12 @@
+document.write('hello <br>');
+
+document.write( '<h>' + 'hello' + '</h>'+'<br>' );
+
+let k1 = 'dfgdfgdfg'
+document.write(k1);
+
+document.write('1 <br>');
+document.write('2 <br>');
+document.write('3 <br>');
+document.write('4 <br>');
+document.write('5 <br>');
