40f55d4919ef607e15c91f947b2a9eb3fd8c135a
Alan <bulgakov.alan@list.ru>
2023-03-10 18:29:41 +0300

    «j41»

---
+++ 41j/1.js
@@ -0,0 +1,19 @@
+let a1=[f, g, d];
+a1[0]=a;
+a1[1]=b;
+a1[2]=c;
+console.log(a1);
+
+//z2
+let a2=[f, g, d];
+a2[0]+=a;
+a2[1]+=b;
+a2[2]+=c;
+console.log(a1);
+
+//z3
+let a3=[1, 2, 3];
+a3[0]++;
+a3[1]++;
+a3[2]--;
+console.log(a1);
