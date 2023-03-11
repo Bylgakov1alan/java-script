2c477cb773fe47a0d040295ff76d26cd1b9646f7
Alan <bulgakov.alan@list.ru>
2023-03-11 13:13:12 +0300

    «j48»

---
+++ 48j/1.js
@@ -0,0 +1,8 @@
+let a={1: 'понедельник', 2: 'вторник', 3: 'среда', 4: 'четверг', 5: 'пятница', 6: 'суббота', 7: 'воскресенье'}
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+console.log(a[7]);
