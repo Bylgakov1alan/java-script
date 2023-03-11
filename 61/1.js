7671910b25176469d4b44a5e69a3fcdd379f9340
Alan <bulgakov.alan@list.ru>
2023-03-11 22:59:26 +0300

    «j61»

---
+++ 61/1.js
@@ -0,0 +1,27 @@
+// let key = 'a';
+// let obj = {
+// 	[key + '1']: 1,
+// 	[key + '2']: 2,
+// 	[key + '3']: 3
+// };
+// console.log(obj);
+
+//z1
+let key = 'x';
+
+let obj = {
+	[key]: 1,
+	y: 2,
+	z: 3
+};
+console.log(obj);
+//z2
+let key1 = 'x';
+let key2 = 'y';
+let key3 = 'z';
+let o = {
+	[key1]: 1,
+	[key2]: 2,
+	[key3]: 3
+};
+console.log(o);
