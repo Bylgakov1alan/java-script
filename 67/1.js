33402f4e6ee287cff53386061fa339a53e1ad5cb
Alan <bulgakov.alan@list.ru>
2023-03-12 11:19:08 +0300

    «j67»

---
+++ 67/1.js
@@ -0,0 +1,24 @@
+//z1 object
+let test = {x: 1, y: 2, z: 3};
+console.log(test);
+//z2 number
+test = {x: 1, y: 2, z: 3};
+console.log(test.x);
+//z3 array
+test = [1, 2, 3];
+console.log(test);
+//z4 number
+test = [1, 2, 3];
+console.log(test[1]);
+//z5 array
+let test1 = [1, 2, 3];
+let test2 = 1;
+
+console.log(test1);
+//z6 number
+test1 = [1, 2, 3];
+test2 = 1;
+
+console.log(test1[test2]);
+
+// number boolean bigint string undefined symbol null 
