4ac51c7d6240ab659128e792a7eac5515a0069a4
Alan <bulgakov.alan@list.ru>
2023-03-12 12:58:47 +0300

    «j71»

---
+++ 71/1.js
@@ -0,0 +1,10 @@
+let obj = {x: 1, y: 2, z: 3};
+console.log(obj.x);
+//z2
+let key = 'x';
+console.log(obj[key]);
+//z3
+let sum = obj.x + obj.y + obj.z;
+console.log(sum);
+//z4
+console.log(Object.keys(obj).length);
