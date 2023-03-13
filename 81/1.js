150990cc29084ff11ebaae757c3e7f8e991ffd00
Alan <bulgakov.alan@list.ru>
2023-03-13 12:35:17 +0300

    «j81»

---
+++ 81/1.js
@@ -0,0 +1,25 @@
+let a=9;
+let b=10;
+if(a>0 && b>0)
+{
+ alert(a+'and'+b+'>0');
+}
+else{
+ alert(a+'and'+b+'<0');
+}
+//z2
+if(b>=10 && b<=20)
+{
+ alert(b+'b>=10 and b<=20');
+}
+else{
+ alert(b+'b<=10 and b>=20');
+}
+//z3
+if(a==1 && a<=1)
+ {
+  if(b>3 && b==3)
+ {
+  alert('+++');
+ }
+ }
