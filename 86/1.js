5ec68910a4c3495017990a48f23ee92b6635e6ca
Alan <bulgakov.alan@list.ru>
2023-03-13 12:54:29 +0300

    «j86»

---
+++ 86/1.js
@@ -0,0 +1,17 @@
+let test = true;
+
+if (test === true) 
+{
+	console.log('+++');
+} else 
+{
+	console.log('---');
+}
+//z2
+if (test === false) 
+{
+	console.log('+++');
+} else 
+{
+	console.log('---');
+}
