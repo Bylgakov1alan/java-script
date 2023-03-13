f52f84f00980d55d7217a077e49f467171752c0e
Alan <bulgakov.alan@list.ru>
2023-03-13 13:46:10 +0300

    «j87»

---
+++ 87/1.js
@@ -0,0 +1,26 @@
+//z2 +++
+//z3 ---
+//z4 ---
+//z5 +++
+//z6 +++
+//z7 +++
+//z8 ---
+
+//z1
+Boolean(false);         // false
+Boolean(undefined);     // false
+Boolean(null);          // false
+Boolean('');            // false
+Boolean(NaN);           // false
+Boolean(0);             // false
+Boolean(-0);            // false
+Boolean(0n);            // false
+
+// Boolean(true);          // true
+// Boolean('hi');          // true
+// Boolean(1);             // true
+// Boolean([]);            // true
+// Boolean([0]);           // true
+// Boolean([1]);           // true
+// Boolean({});            // true
+// Boolean({ a: 1 });      // true
