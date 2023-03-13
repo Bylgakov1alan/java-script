28c43f71fdf44a749510dff78ef54107aa5a4bd2
Alan <bulgakov.alan@list.ru>
2023-03-13 13:49:34 +0300

    «j89»

---
+++ 89/1.js
@@ -0,0 +1,20 @@
+//z1
+let test = true;
+
+if (test) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+//z2
+if (!test) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+//z3
+if (!test) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
