292cc290a4075ccc657a88de8852498280d6c6e1
Alan <bulgakov.alan@list.ru>
2023-03-18 21:34:46 +0300

    101

---
+++ 101/1.js
@@ -0,0 +1,20 @@
+let age = 17;
+
+if (age >= 18) {
+	let adult = true;
+ console.log(adult);
+} else {
+	let adult = false;
+ console.log(adult);
+}
+
+// let age = 17;
+// let adult
+// if (age >= 18) {
+// adult = true;
+//
+// } else {
+//adult = false;
+//
+// }
+  // console.log(adult);
