f8343c44e8857a147b3dbd6edf04ffbac668c94f
Alan <bulgakov.alan@list.ru>
2023-03-18 22:17:33 +0300

    102

---
+++ 102/1.js
@@ -0,0 +1,59 @@
+//z1 
+// let age = 17;
+// let adult;
+
+// if (age >= 18) {
+// 	adult = true;
+// } else {
+// 	adult = false;
+// }
+
+// console.log(adult);
+
+
+//z2
+// let age = 17;
+// let adult;
+
+// if (age >= 18) {
+// 	adult = true;
+// } else {
+// 	adult = false;
+// }
+
+// console.log(adult);
+
+//z3
+// let age = 17;
+// let res;
+
+// if (age >= 18) {
+// 	if (age <= 23) {
+// 		res = 'от 18 до 23';
+// 	} else {
+// 		res = 'больше 23';
+// 	}
+// } else {
+// 	res = 'меньше 18';
+// }
+
+// console.log(res);
+
+
+//z4
+let age = 19;
+let res;
+
+if (age >= 18) {
+	
+	
+	if (age <= 23) {
+		res = 'от 18 до 23';
+	} else {
+		res = 'больше 23';
+	}
+} else {
+	res = 'меньше 18';
+}
+
+console.log(res);
