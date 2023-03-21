b18b70f2d952eca80fae96c552f25ed8eea2e7a0
Alan <bulgakov.alan@list.ru>
2023-03-21 10:16:10 +0300

    106

---
+++ 106/1.js
@@ -0,0 +1,21 @@
+//z1
+// let a=13415234;
+// let f=String(a);
+// if(f[f.length-1]=='0')
+// {
+//  console.log(' yes =');
+// }
+// else{
+//  console.log('no !=');
+// }
+
+//z2
+let a=34543252547;
+let f=String(a);
+if(f[f.length-1]==0 || f[f.length-1]==2 || f[f.length-1]==4 ||f[f.length-1]==6 ||f[f.length-1]==8 )
+{
+ console.log('число четное');
+}
+else{
+ console.log('ничетное число');
+}
