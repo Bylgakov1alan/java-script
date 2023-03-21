b29c8e487776333d3c24e2fda0929cb41062af6b
Alan <bulgakov.alan@list.ru>
2023-03-21 13:26:40 +0300

    107

---
+++ 107/1.js
@@ -0,0 +1,18 @@
+let a=6;
+let b=7;
+let c=2;
+//z1 
+if(a%c==0)
+{
+ console.log("четное число");
+}
+else{
+ console.log("нечетное число");
+}
+if(a%3==0)
+{
+ console.log('делится на 3');
+}
+else{
+ console.log('не делится на 3')
+}
