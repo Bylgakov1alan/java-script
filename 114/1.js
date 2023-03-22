683bbcb5fb817743fafb4ea9033c1c2073ccf6ac
Alan <bulgakov.alan@list.ru>
2023-03-22 18:39:53 +0300

    114

---
+++ 114/1.js
@@ -0,0 +1,23 @@
+let a=0;
+while(a>=100)
+{
+ a=a+1;
+ console.log(a);
+}
+
+//z2
+a=11;
+while(a>=33)
+{
+ a=a+1;
+ console.log(a);
+}
+
+
+//z3
+a=1;
+while(a>=1000)
+{
+ a=a*3;
+ console.log(a);
+}
