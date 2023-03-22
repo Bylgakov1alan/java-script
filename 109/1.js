b183c1e8f9b1b082ad5e2210c3bbb246a9d12543
Alan <bulgakov.alan@list.ru>
2023-03-22 09:00:18 +0300

    109

---
+++ 109/1.js
@@ -0,0 +1,62 @@
+//z1
+// let m=prompt('введите месяц');
+// if(m==12 || m==1 || m==2)
+// {
+//  console.log('зима');
+// }
+// if(m>=3 && m<=5)
+// {
+//  console.log('весна');
+// }
+// if(m>=6 && m<=8)
+// {
+//  console.log('лето');
+// }
+// if(m>=9 && m<=11)
+// {
+//  console.log('осень');
+// }
+
+
+
+//z2
+// let f='asddsfsdf';
+// if(f[0]=='a')
+// {
+//  console.log('yes');
+// }
+// else{
+//  console.log('no');
+// }
+
+
+//z3
+// let f= 1123431;
+// if(f[0]==1 || f[0]==1 || f[0]==3)
+// {
+//  console.log('yes');
+// }
+// else{
+//  console.log('no');
+// }
+
+//z4
+// let f=546;
+// let g=String(f);
+// let sum= Number(g[0])+Number(g[1])+Number(g[2]);
+// console.log(sum);
+
+
+
+//z5
+let f=546567;
+let g=String(f);
+let sum= Number(g[0])+Number(g[1])+Number(g[2]);
+let sum1= Number(g[3])+Number(g[4])+Number(g[5]);
+if(sum == sum1)
+{
+ console.log('да');
+}
+else{
+ console.log('нет');
+}
