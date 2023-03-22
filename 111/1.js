c18a3e3907c93f80662a65ee83b5dcffc83241a8
Alan <bulgakov.alan@list.ru>
2023-03-22 09:10:37 +0300

    111

---
+++ 111/1.js
@@ -0,0 +1,28 @@
+//z1
+// for(let i=0; i<=100; i++)
+// {
+//  console.log(i);
+// }
+
+
+
+//z2
+// for(let i=11; i<=33; i++)
+// {
+//  console.log(i);
+// }
+
+//z3
+// for( let i=0; i<=100; i++)
+// if(i%2==0)
+// console.log(i);
+
+
+//z4
+// for( let i=0; i<=99; i++)
+// if(i%2!=0)
+// console.log(i);
+
+//z5
+for( let i=100; i>0; i--)
+console.log(i);
