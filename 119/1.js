f89beb51b1c7c74fd39c59586351179c285018a7
Alan <bulgakov.alan@list.ru>
2023-03-24 17:43:24 +0300

    119

---
+++ 119/1.js
@@ -0,0 +1,32 @@
+// let s='';
+// for(let i = 0; i<=5; i++)
+// {
+//  s+=' ';
+// }
+// console.log(s);
+
+
+//z2
+// let s='';
+// for(let i = 0; i<=9; i++)
+// {
+//  s+=i;
+// }
+// console.log(s);
+
+//z3
+// let s='';
+// for(let i = 9; i<=1; i++)
+// {
+//  s+=i;
+// }
+// console.log(s);
+
+//z4
+let s='';
+for(let i = 0; i<=9; i++)
+{
+ s+=i;
+ s+='-';
+}
+console.log(s);
