d958b3ea1b7450f4c67396fe37c54e0bd59f9171
Alan <bulgakov.alan@list.ru>
2023-03-24 16:28:28 +0300

    115

---
+++ 115/1.js
@@ -0,0 +1,28 @@
+//z1
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// for(let i=0; i<=arr.length-1; i++)
+// {
+//  console.log(arr[i]);
+// }
+
+//z2
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// for(let i=1; i<arr.length-1; i++)
+// {
+//  console.log(arr[i]);
+// }
+
+//z3
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// for(let i=arr.length-1; i<=0 i++)
+// {
+//  console.log(arr[i]);
+// }
+
+
+//z4
+let arr = ['a', 'b', 'c', 'd', 'e'];
+for (let i = 0; i <= arr.length-1; i++) 
+{
+	console.log(arr[i]);
+}
