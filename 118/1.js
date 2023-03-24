05582a1bf9fc759bf749d9d79eb0b0e1d66434c8
Alan <bulgakov.alan@list.ru>
2023-03-24 17:28:26 +0300

    118

---
+++ 118/1.js
@@ -0,0 +1,36 @@
+// let a=0;
+// let rr = [2, 5, 9, 3, 1, 4];
+// for(let i=0; i<=rr.length-1; i++)
+// {
+//  a=a+rr[i];
+// }
+// console.log(a);
+
+//z2
+// let a=0;
+// let rr = [2, 5, 9, 3, 1, 4];
+// for(let i=0; i<=rr.length-1; i++)
+// {
+//  if(rr[i]%2==0)
+//  {a=a+rr[i];
+// }}
+// console.log(a);
+
+
+//z3
+// let a=0;
+// let rr = [2, 5, 9, 3, 1, 4];
+// for(let i=0; i<=rr.length-1; i++)
+// {
+//  a=a+(rr[i]*rr[i]);
+// }
+// console.log(a);
+
+//z4
+let a=1;
+let rr = [2, 5, 9, 3, 1, 4];
+for(let i=0; i<=rr.length-1; i++)
+{
+ a=a*rr[i];
+}
+console.log(a);
