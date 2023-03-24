b2086181e8d2a3abe0d7b3a26344eb09b0b3d106
Alan <bulgakov.alan@list.ru>
2023-03-24 16:36:58 +0300

    116

---
+++ 116/1.js
@@ -0,0 +1,16 @@
+let arr = [2, 5, 9, 15, 1, 4];
+for(let i=0; i<=arr.length-1; i++)
+{
+ if(arr[i]>=3 && arr[i]<=10)
+ console.log(arr[i]);
+}
+
+
+//z2
+let obj = {a: 1, b: 2, c: 3, d: 4, e: 5};
+
+for(let i=0; i<=obj.length-1; i++)
+{
+ if(obj[i]%2==0)
+ console.log(obj[i]);
+}
