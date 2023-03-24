121be6b8ead699196fa5763872490c478ad7b4ed
Alan <bulgakov.alan@list.ru>
2023-03-24 17:20:45 +0300

    117

---
+++ 117/1.js
@@ -0,0 +1,22 @@
+let a=0;
+// for(let i=0; i<=100; i=i+2)
+// {
+//  a=a+i;
+// }
+// console.log(a);
+
+//z2
+// a=0;
+// for(let i=1; i<=99; i=i+2)
+// {
+//  a=a+i;
+// }
+// console.log(a);
+
+//z3
+a=1;
+for(let i=1; i<=20; i++)
+{
+ a=a*i;
+}
+console.log(a);
