06bb767e0d28e804718171769ee91ed186903d0d
Alan <bulgakov.alan@list.ru>
2023-03-29 08:40:43 +0300

    121

---
+++ 121/1.js
@@ -0,0 +1,47 @@
+// let a= [1,2,3,4,5,6,7,8,9,0,4,34,5];
+// for(let i of a)
+// {
+//  if(i==0)
+//  {
+//   console.log('yes');
+//   break;
+//  }
+// }
+
+//z2
+// let a=[1,2,3,4,5,6,7,-6,-9,-5];
+// let sum=0;
+// for(let i of a)
+// {
+//  if(a[i]>=0)
+//  {
+//   sum=sum+a[i];
+//  }
+//  else
+//  {
+//   break;
+//  }
+// }
+// console.log(sum);
+
+//z3
+// let a = [1,2,5,6,7,8,3];
+// for(let i of a)
+// {
+//  if(a[i]==3)
+//  {
+//   console.log(i);
+//   break;
+//  }
+// }
+
+//z4
+for(let i=0; i<=100; i++)
+{
+ sum=sum+i;
+ if(sum>=100)
+ {
+  console.log(sum);
+  break;
+ }
+}
