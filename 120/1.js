b7bd58a7ea44fd5b99ddbde0c740603046a84d2d
Alan <bulgakov.alan@list.ru>
2023-03-29 08:12:16 +0300

    120

---
+++ 120/1.js
@@ -0,0 +1,34 @@
+// for(let i = 1; i<1000; i++)
+// {
+//  let str=1;
+//  str = String(i);
+//  console.log(str[0]);
+// }
+
+//z2
+// for(let i = 10; i<1000; i++)
+// {
+//  let str=1;
+//  str = String(i);
+//  let sum= Number(str[0])+Number(str[1]);
+//  console.log(sum);
+// }
+
+//z3
+// for(let i = 10; i<1000; i++)
+// {
+//  let str=1;
+//  str = String(i);
+//  if( str[0]==1)
+//  console.log(str);
+// }
+
+//z4
+for(let i = 10; i<1000; i++)
+{
+ let str=1;
+ str = String(i);
+ let sum= Number(str[0])+Number(str[1]);
+ if(sum==5)
+ console.log(sum);
+}
