d0e9c4540a8419d458e41c3aaf182d38b4c29e05
Alan <bulgakov.alan@list.ru>
2023-04-03 10:31:55 +0300

    123

---
+++ 123/1.js
@@ -0,0 +1,17 @@
+// for(let i=0; i<=9; i++)
+// {
+//  for(let j=1; j<=3; j++)
+//  {
+//   console.log(i);
+//  }
+// }
+
+
+//z2
+for(let i=0; i<=3; i++)
+{
+ for(let j=1; j<=3; j++)
+ {
+  console.log(i*10+j);
+ }
+}
