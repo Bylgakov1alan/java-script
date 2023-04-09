112f8e6de9b660d46f7e841eac25090bc6e017f1
Alan <bulgakov.alan@list.ru>
2023-04-09 19:01:12 +0300

    126

---
+++ 126/1.js
@@ -0,0 +1,28 @@
+// a=[];
+// for(let i=0; i<=10; i++)
+// {
+// a.push(i);
+// }
+// console.log(a);
+
+const { acceptsEncoding } = require("express/lib/request");
+
+//z2
+// a=[];
+// for(let i=0; i<=10; i++)
+// {
+// a.push('x');
+// }
+// console.log(a);
+
+//z3
+let a=[1,2,3,4,-5,6,-1,8];
+let b=[];
+for(let i=0; i<=length; i++)
+{
+ if(a[i]>0)
+ {
+ b[i].push(a);
+ }
+}
+console.log(b);
