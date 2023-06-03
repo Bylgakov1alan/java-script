ba69c23e5a9d1ec47b6678a5947183993b32c88d
Alan <bulgakov.alan@list.ru>
2023-06-03 12:04:10 +0300

    132

---
+++ 132/1.js
@@ -0,0 +1,18 @@
+// let a = 2*(3-1);
+// let b = 6-2;
+
+// let result = a == b ?true: false;
+// console.log(result);
+
+//z2
+// let a = 5 * (7 - 4);
+// let b = 1 + 2 + 7;
+// let result = a > b ?true: false;
+// console.log(result);
+
+//z3
+let a = 2 ** 4;
+let b = 4 ** 2;
+
+let result = a != b ? true: false;
+console.log(result);
