e6d5a1585e0ef86990eb3303cd10145b4c997ae7
Alan <bulgakov.alan@list.ru>
2023-06-09 12:20:03 +0300

    136

---
+++ 136/1.js
@@ -0,0 +1,13 @@
+let min = 30;
+
+if (min >= 0 && min <= 20) {
+	console.log('1 четверть');
+}
+
+if (min >= 21 && min <= 40) {
+	console.log('2 четверть');
+}
+
+if (min >= 41 && min <= 60) {
+	console.log('3 четверть');
+}
