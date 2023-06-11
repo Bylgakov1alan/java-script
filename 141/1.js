c3dc9b86d60e9c929c33f7f5e8efcd99c4cef56c
Alan <bulgakov.alan@list.ru>
2023-06-11 23:08:01 +0300

    141

---
+++ 141/1.js
@@ -0,0 +1,39 @@
+// let num1 = 1;
+// let num2 = 2;
+
+// if (num1 + num2 == 3) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+//z2
+// let num = 123;
+// let g=String(num)
+// if (g[0] == 1) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+
+//z3
+// let num = 12;
+// let g=String(num);
+// if (g.length == 2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+//z4
+let num = '123033'; 
+
+let sum1 = num[0] + num[1] + num[2];
+let sum2 = num[3] + num[4] + num[5];
+
+if (sum1 == sum2) {
+	console.log('суммы равны');
+} else {
+	console.log('суммы не равны');
+}
