5c74a1d966dfc577af42cebff07ed153e131525c
Alan <bulgakov.alan@list.ru>
2023-06-11 20:46:37 +0300

    139

---
+++ 139/1.js
@@ -0,0 +1,20 @@
+// let a=123235500;
+
+// if(String(a).length-1=='0')
+// {
+//  console.log("YES");
+// }
+// else{
+//  console.log('NO');
+// }
+
+//z2
+let a=123235500;
+
+if(String(a).length-1=='0' || String(a).length-1=='2' || String(a).length-1=='4' || String(a).length-1=='6' || String(a).length-1=='8')
+{
+ console.log("YES");
+}
+else{
+ console.log('NO');
+}
