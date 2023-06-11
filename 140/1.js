e448cadd7367f79ba5237b778e7cf54f61ebbeb9
Alan <bulgakov.alan@list.ru>
2023-06-11 20:48:46 +0300

    140

---
+++ 140/1.js
@@ -0,0 +1,17 @@
+// let a=450;
+// if (a%2==0)
+// {
+//  console.log("четное");
+// }
+// else{
+//  console.log("нечетное");
+// }
+//z2
+let a=450;
+if (a%3==0)
+{
+ console.log("делится");
+}
+else{
+ console.log("не делится");
+}
