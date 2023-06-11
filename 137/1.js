5dad385021eb76751fd4c09574c3f1db8ba13299
Alan <bulgakov.alan@list.ru>
2023-06-11 20:41:41 +0300

    138

---
+++ 137/1.js
@@ -0,0 +1,5 @@
+let str = '123';
+
+if (str.length == 3) {
+	console.log(Number(str[0])+Number(str[1])+Number(str[2]));
+}
---
+++ 138/1.js
@@ -0,0 +1,29 @@
+// let str = 'asdfasdv';
+
+// if (str[0] == 'a') {
+// 	console.log("YES");
+// }
+// else {
+//  console.log("NO")
+// }
+
+//z2
+// let str = 'asdfasdvx';
+
+// if (str.length == 'x') {
+// 	console.log("YES");
+// }
+// else {
+//  console.log("NO")
+// }
+
+
+//z3
+let str = 'asdfasdv';
+
+if (str[0] == 'a' || str[0]=='b') {
+	console.log("YES");
+}
+else {
+ console.log("NO")
+}
