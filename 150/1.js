d562b231831d846c69259d2bb73bda375bcd6a3d
Alan <bulgakov.alan@list.ru>
2023-06-12 12:58:25 +0300

    150

---
+++ 150/1.js
@@ -0,0 +1,31 @@
+// let a=2;
+// let b=0;
+// for (a=2; a<=100; a++){
+// if (a%2==0){
+
+
+//  b=a+b;}
+// }
+// console.log(b);
+
+//z2
+// let a=2;
+// let b=0;
+// for (a=2; a<=100; a++){
+// if (a%2!=0){
+
+
+//  b=a+b;}
+// }
+// console.log(b);
+
+//z3
+let a=1;
+let b=0;
+for (a=1; a<=20; a++){
+if (a%2==0){
+
+
+ b=a+b;}
+}
+console.log(b);
