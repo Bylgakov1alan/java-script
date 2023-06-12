9205ec90a192bc1fbba7b308b42812394f45787c
Alan <bulgakov.alan@list.ru>
2023-06-12 21:24:05 +0300

    153

---
+++ 153/1.js
@@ -0,0 +1,33 @@
+// for (let i = 10; i <= 1000; i++) {
+// 	let str = String(i); 
+	
+//	console.log(str[0]);
+//  }
+
+
+ //z2
+// for (let i = 10; i <= 1000; i++) {
+// 	let str = String(i); 
+// 	let a=Number(str[0])+Number(str[1]);
+// 		console.log(a);
+// }
+
+
+//z3
+for (let i = 10; i <= 1000; i++) {
+	let str = String(i); 
+	
+	if (str[0] === '1') { 
+		console.log(i);
+	}
+}
+
+//z5
+for (let i = 10; i <= 1000; i++) {
+	let str = String(i); 
+	
+	if (str[0]+str[1] 
+		==5) { 
+		console.log(i);
+	}
+}
