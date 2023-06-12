6d037aa285faf312899aaedb9c778647ee29ccb1
Alan <bulgakov.alan@list.ru>
2023-06-12 11:17:39 +0300

    147

---
+++ 147/1.js
@@ -0,0 +1,34 @@
+//z1
+// было
+
+// //z2
+// было
+
+//z3
+// let a=0;
+// for (a=0; a<=100 a++){
+//  console.log(a);
+// }
+
+//z4
+// let a=0;
+// for (a=0; a<=100 a++){
+ // if(a%2==0){
+ // console.log(a);
+ // }
+// }
+
+
+//z5
+// let a=1;
+// for (a=1; a<=99; a--){
+//  if(a%2!=0){
+//  // console.log(a);
+//  // }
+// }
+
+//z6
+let a=100;
+for (a=100; a<=1; a--){
+ console.log(a);
+}
