b4b1c60fa987176cc4cdb49cd29f1599d1f05026
Alan <bulgakov.alan@list.ru>
2023-06-12 10:46:01 +0300

    142

---
+++ 142/1.js
@@ -0,0 +1,59 @@
+// let m=7;
+// if (m==12 || m==1 ||m==2){
+//  console.log("ЗИМА");
+// }
+// else if( m>=3 || m<=5)
+// {
+//  console.log("ВЕСНА");
+// }
+// else if (m>=6 || m<=8){
+//  console.log("ЛЕТО");
+// }
+// else if (m>=9 || m<=11){
+//  console.log("ОСЕНЬ");
+// }
+// else{
+//  console.log("такого месяца не было");
+// }
+
+
+//z2
+// let a='abcde';
+// if (a[0]=='a'){
+//  console.log("YES");
+// }
+// else{
+//  console.log("NO")
+// }
+
+
+//z3
+// let a=123456;
+// let chislo=String(a);
+// if (chislo[0]=='1' || chislo[0]=='2' || chislo[0]=='3'){
+//  console.log("YES");
+// }
+// else{
+//  console.log("NO")
+// }
+
+
+//z4
+// let a=123;
+// let t=a%10;
+// let v=a/10%10;
+// let p=a/100;
+// console.log(t+p+v);
+
+
+//z5
+let num = '123033'; 
+
+let sum1 = num[0] + num[1] + num[2];
+let sum2 = num[3] + num[4] + num[5];
+
+if (sum1 == sum2) {
+	console.log('суммы равны');
+} else {
+	console.log('суммы не равны');
+}
