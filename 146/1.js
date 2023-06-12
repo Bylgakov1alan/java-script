13be2934d43555ee444f0999f3b7950681c627e3
Alan <bulgakov.alan@list.ru>
2023-06-12 11:12:03 +0300

    146

---
+++ 146/1.js
@@ -0,0 +1,22 @@
+// let num=1;
+// while (num<=100){
+//  console.log(num);
+//  num=num+1;
+// }
+
+//z2
+// let num=11;
+// while (num<=33){
+//  console.log(num);
+//  num=num+1;
+// }
+
+//z3
+let iter=0;
+let num=1;
+while (num<=1000){
+ console.log(num);
+ num=num*3;
+ iter=iter+1
+}
+console.log(iter);
