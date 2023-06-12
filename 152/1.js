2b123e3c3ec750bd5416d38d817d347fcda4725a
Alan <bulgakov.alan@list.ru>
2023-06-12 20:55:16 +0300

    152

---
+++ 152/1.js
@@ -0,0 +1,29 @@
+let a='';
+for (let i=0; i<=4; i++){
+ a+='-';
+}
+console.log(a);
+
+//z2
+let b='';
+for (let i=1; i<=9; i++){
+ b+=i;
+}
+console.log(b);
+
+//z3
+let c='';
+for (let i=9; i>=1; i--){
+ c+=i;
+}
+console.log(c);
+
+
+//z4
+let g='';
+for (let i=1;i<=9;i++){
+ g+='-';
+ g+='i';
+}
+g+='-';
+console.log(g);
