fef666f19caf7bd3d20bc5681371b2361792470f
Alan <bulgakov.alan@list.ru>
2023-06-12 11:28:03 +0300

    148

---
+++ 148/1.js
@@ -0,0 +1,25 @@
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// for (let i = 0; i < arr.length; i++) {
+// 	console.log(arr[i]);
+// }
+
+//z2
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// for (let i = 1; i < arr.length-2; i++) {
+// 	console.log(arr[i]);
+// }
+
+
+//z3
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// for (let i = arr.length-2; i <= 0; i--) {
+// 	console.log(arr[i]);
+// }
+
+
+//z4
+let arr = ['a', 'b', 'c', 'd', 'e'];
+		
+for (let i = 0; i <= arr.length-1; i++) {
+	console.log(arr[i]);
+}
