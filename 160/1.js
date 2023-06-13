77361334f0ffdc07a53bb89d95fe2ff827213de8
Alan <bulgakov.alan@list.ru>
2023-06-13 11:52:20 +0300

    160

---
+++ 160/1.js
@@ -0,0 +1,29 @@
+// let arr = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < arr.length; 
+// 	i++) { 
+// 	arr[i]= arr[i]**2;
+// }
+
+// console.log(arr); 
+
+
+//z2
+// let arr = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < arr.length; 
+// 	i++) { 
+// 	arr[i]= arr[i]-1;
+// }
+
+// console.log(arr); 
+
+
+let arr = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < arr.length; 
+	i++) { 
+	arr[i]= arr[i]+10;
+}
+
+console.log(arr);
