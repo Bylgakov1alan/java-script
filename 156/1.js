b95d3651efb433f6e06064efcef851e4f142cc84
Alan <bulgakov.alan@list.ru>
2023-06-13 09:31:34 +0300

    156

---
+++ 156/1.js
@@ -0,0 +1,20 @@
+// let a=1;
+// let b=1;
+// for (a=1; a<=9;a++){
+//  for (b=1;b<=3;b++)
+//  console.log(a);
+// }
+
+
+//z2
+let a=1;
+let b=1;
+let g='';
+for (a=1; a<=3;a++){
+ for (b=1;b<=3;b++)
+ {
+ g=String(a)+String(b);
+ console.log(g);
+ g='';
+ }
+}
