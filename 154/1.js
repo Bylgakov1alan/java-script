d76f67d9a9051b096bb8ce7886b4899325b09c6c
Alan <bulgakov.alan@list.ru>
2023-06-13 09:25:32 +0300

    154

---
+++ 154/1.js
@@ -0,0 +1,42 @@
+// let arr = [1, 2, 3, 4, 5];
+
+// for (let elem of arr) {
+// 	if (arr[elem] == 0) {
+// 		console.log(arr[elem]);
+// 		break; 
+// 	}}
+
+
+ //z2
+// let arr = [1, 2, 3, 4, 5];
+
+// for (let elem of arr) {
+// 	if (arr[elem] >= 0) {
+// 		console.log(arr[elem]); 
+// 	}
+// else{
+//  break;
+// }}
+
+
+//z3
+// let arr = [1, 2, 3, 4, 5];
+
+// for (let elem of arr) {
+// 	if (arr[elem] == 3) {
+// 		console.log(elem); 
+// 	}
+// }
+
+
+//z4
+let a=1;
+let sum=0;
+let chislo=0;
+for(a=1;a<=100;a++){
+ sum+=a;
+ chislo++;
+ if (a>=100){
+  console.log(chislo);
+ }
+}
