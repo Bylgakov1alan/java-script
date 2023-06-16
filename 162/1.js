cbf61f44eb473aa365e8c0d63c906a83003a5f5e
Alan <bulgakov.alan@list.ru>
2023-06-16 15:49:30 +0300

    j162

---
+++ 162/1.js
@@ -0,0 +1,14 @@
+let obj = {x: 1, y: 2, z: 3};
+
+for (let i=0; i<=obj.length; i++){
+ obj[i]=obj[i]**2;
+
+}
+console.log(obj);
+
+//z2
+let a = {x: 1, y: 2, z: 3};
+for (let i=0; i<=a.length; i++){
+ a[i]=a[i]+1;
+}
+console.log(a);
