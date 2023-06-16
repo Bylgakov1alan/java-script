2d1a69693f1ab8c6e7493995f2377a63f2638748
Alan <bulgakov.alan@list.ru>
2023-06-16 18:18:42 +0300

    j170

---
+++ 170/1.js
@@ -0,0 +1,20 @@
+let arr = [
+ [1, 2, 3, 
+  [4, 5, 
+   [6, 7]
+  ]
+ ], 
+ [8, 
+  [9, 10]
+ ]
+]; 
+
+let sum = 0;
+
+arr = arr.flat(Infinity)
+
+for (let i = 0; i < arr.length; i++) {
+     sum += arr[i];
+}
+
+console.log(sum);
