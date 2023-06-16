e518a55e69b0c2445ac6d58605c8141b39dea1e4
Alan <bulgakov.alan@list.ru>
2023-06-16 15:41:06 +0300

    j161

---
+++ 161/1.js
@@ -0,0 +1,36 @@
+// let arr1 = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+// let arr2 = [1, 2, 3, 4, 5, 6, 7];
+// let obj = {};
+
+// for (let i = 0; i <= 4; i++) {
+// 	let key = arr1[i];
+// 	let value = arr2[i];
+	
+// 	obj[key] = value;
+// }
+
+// console.log(obj);
+
+//z2
+// let obj = {a: 1, b: 2, c: 3, d: 
+// 	4, e: 5}; 
+// 	let a=[];
+//  for (let i=0;i<=obj.lengh; i++){
+// 		if( obj[i]){
+// 			a.push(obj[i]);
+// 		}
+// 		else{
+// 			continue;
+// 		}
+// 	}
+// 	console.log(a);
+
+
+	//z3
+	let obj = {a: 1, b: 2, c: 3, d: 
+	4, e: 5}; 
+
+	for(let i=0; i<=obj.lengh; i++){
+		let a=obj[i]
+		
+	}
